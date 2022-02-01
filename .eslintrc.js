20bd93ad3beecf46a3732d3f4e30cf6c659234c2
burczu <b .dybowski@gmail.com>
2022-02-01 13:01:18 +0100

    prettier added and configured

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  rules: {
+    'import/named': 'off',
+    'no-unused-vars': 'off',
+    'no-case-declarations': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': [2, { ignore: ['children'] }],
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      {
+        vars: 'all',
+        args: 'after-used',
+      },
+    ],
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'prettier/prettier': ['error', { singleQuote: true }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', '.'],
+      },
+      typescript: {},
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+  globals: {
+    console: true,
+    module: true,
+    process: true,
+    require: true,
+    setTimeout: true,
+    window: true,
+    document: true,
+    sessionStorage: true,
+    history: true,
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,24 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import React from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>Cats App v1.0</title>
-        <meta name="description" content="Cats App is pet project to test some new things..." />
+        <meta
+          name="description"
+          content="Cats App is pet project to test some new things..."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>
-          Cats App v1.0
-        </h1>
+        <h1>Cats App v1.0</h1>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
