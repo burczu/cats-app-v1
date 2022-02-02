5cdc054c0ac0fbf65f59e96a3190f871ad432d3b
burczu <b .dybowski@gmail.com>
2022-02-02 14:15:01 +0100

    cats model added

---
+++ model/cats/hook.tsx
@@ -0,0 +1,15 @@
+import { ICatsHook } from './types';
+import { useCatsContext } from '../../context/cats';
+
+const useCats = (): ICatsHook => {
+  const { setCats } = useCatsContext();
+  const initCats = (initialValue: any[]) => {
+    setCats(initialValue);
+  };
+
+  return {
+    initCats,
+  };
+};
+
+export default useCats;
---
+++ model/cats/types.ts
@@ -0,0 +1,3 @@
+export interface ICatsHook {
+  initCats: (initialValue: any[]) => void;
+}
