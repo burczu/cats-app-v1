84d88713d5e0631d6a1d17076b705e64d6c19e69
burczu <b.dybowski@gmail.com>
2022-02-07 11:07:00 +0100

    caty type added

---
+++ services/cats/types.ts
@@ -0,0 +1,31 @@
+export interface ICat {
+  id: string;
+  name: string;
+  temperament: string;
+  life_span: string;
+  alt_names: string;
+  wikipedia_url: string;
+  origin: string;
+  weight_imperial: string;
+  experimental: number;
+  hairless: number;
+  natural: number;
+  rare: number;
+  rex: number;
+  suppress_tail: number;
+  short_legs: number;
+  hypoallergenic: number;
+  adaptability: number;
+  affection_level: number;
+  country_code: string;
+  child_friendly: number;
+  dog_friendly: number;
+  energy_level: number;
+  grooming: number;
+  health_issues: number;
+  intelligence: number;
+  shedding_level: number;
+  social_needs: number;
+  stranger_friendly: number;
+  vocalisation: number;
+}
