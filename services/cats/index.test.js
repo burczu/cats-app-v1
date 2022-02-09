df13f42b31c1cd6340bb07993518967e22a48d68
burczu <b.dybowski@gmail.com>
2022-02-09 11:46:26 +0100

    pagination added to the get cats list service method

--- services/cats/index.test.js
+++ services/cats/index.test.js
@@ -1,13 +1,22 @@
 import { getCats } from './index';
 import { get } from '@misc/fetch';
+import { DEFAULT_LIST_LIMIT, DEFAULT_LIST_PAGE } from '@misc/constants';
 
 describe('cats service', () => {
-  describe('when getting cats', () => {
-    it('should call get method with correct parameters', () => {
-      const expectedParameter = '/breeds';
+  describe('when getting first page of cats list', () => {
+    it('should call get method with correct default parameters', () => {
+      const expectedParameter = `/breeds?page=${DEFAULT_LIST_PAGE}&limit=${DEFAULT_LIST_LIMIT}`;
 
       getCats();
       expect(get).toHaveBeenCalledWith(expectedParameter);
     });
+  });
+
+  describe('when getting other page of cats list', () => {
+    const expectedPage = 12;
+    const expectedParameter = `/breeds?page=${expectedPage}&limit=${DEFAULT_LIST_LIMIT}`;
+
+    getCats(expectedPage);
+    expect(get).toHaveBeenCalledWith(expectedParameter);
   });
 });
--- services/cats/index.ts
+++ services/cats/index.ts
@@ -1,5 +1,6 @@
 import { get } from '@misc/fetch';
+import { DEFAULT_LIST_PAGE, DEFAULT_LIST_LIMIT } from '@misc/constants';
 
-export const getCats = async () => {
-  return await get('/breeds');
+export const getCats = async (page = DEFAULT_LIST_PAGE) => {
+  return await get(`/breeds?page=${page}&limit=${DEFAULT_LIST_LIMIT}`);
 };
